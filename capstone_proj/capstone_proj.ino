eff4f8cf189be7b235c985e27e63addafe2baed0
Minh Duong <64678096+minhnhat1901@users.noreply.github.com>
2025-11-18 14:59:22 -0800

    Simple code skeleton

--- capstone_proj/capstone_proj.ino
+++ capstone_proj/capstone_proj.ino
@@ -1,9 +1,20 @@
+const int PIR_PIN = 34;
+int motion_status = 0; // current pin reading
+int pir_state = 0; // PIR state change
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(PIR_PIN, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  motionStatus = digitalRead(PIR_PIN);
+  if (motion_status == HIGH) {
 
+    if (pir_state == LOW) {
+      
+    }
+  })
 }
