3470329bcd6fce21a34189d04d10755a89c75142
Minh Duong <64678096+minhnhat1901@users.noreply.github.com>
2025-11-19 15:50:38 -0800

    Speed control the motor

--- capstone_proj/capstone_proj.ino
+++ capstone_proj/capstone_proj.ino
@@ -1,92 +1,110 @@
-#include <DFRobotDFPlayerMini.h>
+// #include <DFRobotDFPlayerMini.h>
 
-const int PIR_PIN = 34;
+// const int PIR_PIN = 34;
 
-bool pir_state = LOW; // PIR state change
-int motion_status = LOW; // current pin reading
+// bool pir_state = LOW; // PIR state change
+// int motion_status = LOW; // current pin reading
 
-DFRobotDFPlayerMini fxPlayer;
+// DFRobotDFPlayerMini fxPlayer;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  delay(500);
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(9600);
+//   delay(500);
 
-  pinMode(PIR_PIN, INPUT);
+//   pinMode(PIR_PIN, INPUT);
 
-  Serial1.begin(9600);
-  delay(200);
+//   Serial1.begin(9600);
+//   delay(200);
 
-  Serial.println("Initializing DFPlayer…");
-  if (!fxPlayer.begin(fxSerial)) {
-    Serial.println("DFPlayer init failed! Check wiring and SD card.");
-    while(true) {}
-  }
+//   Serial.println("Initializing DFPlayer…");
+//   if (!fxPlayer.begin(fxSerial)) {
+//     Serial.println("DFPlayer init failed! Check wiring and SD card.");
+//     while(true) {}
+//   }
   
-  Serial.println("DFPlayer ready.");
+//   Serial.println("DFPlayer ready.");
 
-  fxPlayer.begin(Serial1);
+//   fxPlayer.begin(Serial1);
   
-  // Can set from 0 - 30
-  fxPlayer.volume(20);
-  delay(5000);
+//   // Can set from 0 - 30
+//   fxPlayer.volume(20);
+//   delay(5000);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   motion_status = digitalRead(PIR_PIN);
+
+//   if (motion_status == HIGH && pir_state == LOW) {
+//     Serial.println("Motion Detected");
+//     pir_state = HIGH;
+
+//     // Add below things want to activate
+//     Serial.println("Playing track 1...");
+//     fxPlayer.play(1);
+//     delay(2500);
+//   }
+//   if (motion_status == LOW && pir_state == HIGH) {
+//     Serial.println("Motion Ended");
+//     pir_state = LOW;
+
+//     // Add below things want to deactivate
+//     Serial.println("Playing track 2...");
+//     fxPlayer.play(2);
+//     delay(2200);
+//   }
+
+//   delay(50);
+// }
+
+
+// This code is using to control the clothesline
+const int forwardPin = 2; // --> 2 , 39
+const int backwardPin = 3; // --> 3 , 36
+const int delayTime = 500;
+const int stopTime = 2000;
+
+void speedControl(int speed1, int speed2) {
+  analogWrite(forwardPin, speed1);
+  analogWrite(backwardPin, speed2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  motion_status = digitalRead(PIR_PIN);
-
-  if (motion_status == HIGH && pir_state == LOW) {
-    Serial.println("Motion Detected");
-    pir_state = HIGH;
-
-    // Add below things want to activate
-    Serial.println("Playing track 1...");
-    fxPlayer.play(1);
-    delay(2500);
-  }
-  if (motion_status == LOW && pir_state == HIGH) {
-    Serial.println("Motion Ended");
-    pir_state = LOW;
-
-    // Add below things want to deactivate
-    Serial.println("Playing track 2...");
-    fxPlayer.play(2);
-    delay(2200);
-  }
-
-  delay(50);
+void stopMove() {
+  digitalWrite(forwardPin, LOW);
+  digitalWrite(backwardPin, LOW);
 }
 
+void bringoutMove() {
+  digitalWrite(forwardPin, HIGH);
+  digitalWrite(backwardPin, LOW);
+}
 
-
-/* This code is using to control the clothesline
-const int forwardPin = 39;
-const int backwardPin = 36;
-const int delayTime = 2000;
+void bringinMove() {
+  digitalWrite(forwardPin, LOW);
+  digitalWrite(backwardPin, HIGH);
+}
 
 void setup()
 {
   pinMode(forwardPin, OUTPUT);
   pinMode(backwardPin, OUTPUT);
+  delay(1000);
 }
 
 void loop()
 {
-  digitalWrite(forwardPin, LOW);
-  digitalWrite(backwardPin, LOW);
+  bringoutMove();
+  speedControl(200,0);
   delay(delayTime);
   
-  digitalWrite(forwardPin, HIGH);
-  digitalWrite(backwardPin, LOW);
-  delay(delayTime);
+  stopMove();
+  delay(stopTime);
   
-  digitalWrite(forwardPin, LOW);
-  digitalWrite(backwardPin, LOW);
+  bringinMove();
+  speedControl(0, 200);
   delay(delayTime);
-  
-  digitalWrite(forwardPin, LOW);
-  digitalWrite(backwardPin, HIGH);
-  delay(delayTime);
+
+  stopMove();
+  delay(stopTime);
 }
-*/
