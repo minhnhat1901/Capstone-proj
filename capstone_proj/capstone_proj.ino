3b2f5f23dec8d39f1a7dd2e386a87b2c716227b6
Minh Duong <64678096+minhnhat1901@users.noreply.github.com>
2025-11-21 11:12:43 -0800

    Final version for security door

--- capstone_proj/capstone_proj.ino
+++ capstone_proj/capstone_proj.ino
@@ -1,65 +1,142 @@
-// #include <DFRobotDFPlayerMini.h>
 // #include "motor_control.h"
+#include <DFRobotDFPlayerMini.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h>
 
-// const int PIR_PIN = 34;
+#define SS_PIN 10
+#define RST_PIN 9
 
-// bool pir_state = LOW; // PIR state change
-// int motion_status = LOW; // current pin reading
+const int PIR_PIN = 34;
 
-// DFRobotDFPlayerMini fxPlayer;
+bool pir_state = LOW; // PIR state change
+int motion_status = LOW; // current pin reading
+int count = 0;
 
-// void setup() {
-//   // put your setup code here, to run once:
-//   Serial.begin(9600);
-//   delay(500);
+DFRobotDFPlayerMini fxPlayer;
+Servo myservo;  // create servo object to control a servo
+ 
+MFRC522 mfrc522(53, 49); // Instance of the class
 
-//   pinMode(PIR_PIN, INPUT);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(500);
 
-//   Serial1.begin(9600);
-//   delay(200);
+  pinMode(PIR_PIN, INPUT);
 
-//   Serial.println("Initializing DFPlayer…");
-//   if (!fxPlayer.begin(fxSerial)) {
-//     Serial.println("DFPlayer init failed! Check wiring and SD card.");
-//     return;
-//   }
+  Serial1.begin(9600);
+  delay(2000);
+
+  Serial.println("Initializing DFPlayer…");
+  if (!fxPlayer.begin(Serial1)) {
+    Serial.println("DFPlayer init failed! Check wiring and SD card.");
+    while(true) { delay(1000); }
+  }
   
-//   Serial.println("DFPlayer ready.");
+  Serial.println("DFPlayer ready.");
 
-//   fxPlayer.begin(Serial1);
+  myservo.attach(22);
+  myservo.write(0);
+
+  SPI.begin(); // Init SPI bus
+  mfrc522.PCD_Init(); // Init RC522 
   
-//   // Can set from 0 - 30
-//   fxPlayer.volume(20);
-//   delay(5000);
-// }
+  // Can set from 0 - 30
+  fxPlayer.volume(20);
+  delay(5000);
 
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   motion_status = digitalRead(PIR_PIN);
 
-//   if (motion_status == HIGH && pir_state == LOW) {
-//     Serial.println("Motion Detected");
-//     pir_state = HIGH;
+  Serial.println("Bring your card close to the reader");
+  Serial.println("-----------------------------------");
+}
 
-//     // Add below things want to activate
-//     Serial.println("Playing track 1...");
-//     fxPlayer.play(1);
-//     delay(2500);
-//   }
-//   if (motion_status == LOW && pir_state == HIGH) {
-//     Serial.println("Motion Ended");
-//     pir_state = LOW;
+void loop() {
+  // put your main code here, to run repeatedly:
+  motion_status = digitalRead(PIR_PIN);
 
-//     // Add below things want to deactivate
-//     Serial.println("Playing track 2...");
-//     fxPlayer.play(2);
-//     delay(2200);
-//   }
+  if (motion_status == HIGH && pir_state == LOW) {
+    Serial.println("Motion Detected");
+    count = 0;
+    do {
+      if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial())
+        continue;
 
-//   delay(50);
-// }
+      String card = printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
+
+      if (card == "1E70463") {
+        authorizedAccess();
+        break;
+      } else {
+        accessDenied();
+        count++;
+      }
+
+      Serial.print(F("RFID Tag UID:"));
+      Serial.println(card);
+
+      mfrc522.PICC_HaltA(); // Halt PICC
+    } while (count < 3);
+    
+    if (count == 3) {
+      Serial.println("Playing track 3...");
+      fxPlayer.play(3);
+      delay(2500);
+    }
+    pir_state = HIGH;
+    // Add below things want to activate
+
+  }
+  if (motion_status == LOW && pir_state == HIGH) {
+    Serial.println("Motion Ended");
+    pir_state = LOW;
+
+    // Add below things want to deactivate
+    // Serial.println("Playing track 2...");
+    // fxPlayer.play(2);
+    // delay(2200);
+  }
+
+  delay(50);
+}
+
+//  Routine to dump a byte array as hex values to Serial. 
+String printHex(byte *buffer, byte bufferSize) {
+  String card_id = "";
+
+  for (byte i = 0; i < bufferSize; i++) {
+    card_id.concat(String(buffer[i], HEX));
+  }
+
+  card_id.toUpperCase();
+
+  return card_id;
+}
+
+void authorizedAccess() {
+  Serial.println();
+  Serial.println("Message: Authorized Access");
+  myservo.write(90);
+
+  Serial.println("Playing track 1...");
+  fxPlayer.play(1);
+  delay(3000);
+  myservo.write(0);
+  delay(500);
+}
+
+void accessDenied() {
+  Serial.println();
+  Serial.println("Message: Access Denied");
+  if (count < 2) {
+    Serial.println("Playing track 2...");
+    fxPlayer.play(2);
+    delay(2500);
+  }
+}
 
 // This code is using to control the clothesline
+
 // void setup()
 // {
 //   pinMode(forwardPin, OUTPUT);
@@ -83,80 +160,3 @@
 //   stopMove();
 //   delay(stopTime);
 // }
-
-//Arduino Code - RC522 Read RFID Tag UID
-
-#include <SPI.h>
-#include <MFRC522.h>
-
-// #define SS_PIN 10
-// #define RST_PIN 9
- 
-MFRC522 mfrc522(10, 9); // Instance of the class
-
-// MFRC522::MIFARE_Key key; 
-
-void setup() { 
-  pinMode(6, OUTPUT); pinMode(7, OUTPUT); pinMode(8, OUTPUT);
-  Serial.begin(9600);
-  SPI.begin(); // Init SPI bus
-  mfrc522.PCD_Init(); // Init RC522 
-  Serial.println("Bring your card close to the reader");
-  Serial.println("-----------------------------------");
-}
- 
-void loop() {
-  digitalWrite(6, LOW); digitalWrite(7, LOW); digitalWrite(8, LOW); 
-  if (!mfrc522.PICC_IsNewCardPresent())
-    return;
-
-  if (!mfrc522.PICC_ReadCardSerial())
-    return;
-
-  // MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-
-  Serial.print(F("RFID Tag UID:"));
-  String card = printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
-  Serial.println(card);
-
-  if (card == "1E70463") {
-    authorizedAccess();
-  } else {
-    accessDenied();
-  }
-
-  // mfrc522.PICC_HaltA(); // Halt PICC
-}
-
-//  Routine to dump a byte array as hex values to Serial. 
-String printHex(byte *buffer, byte bufferSize) {
-  String card_id = "";
-
-  for (byte i = 0; i < bufferSize; i++) {
-    card_id.concat(String(buffer[i], HEX));
-  }
-
-  card_id.toUpperCase();
-
-  return card_id;
-}
-
-void authorizedAccess() {
-  Serial.println();
-  Serial.println("Message: Authorized Access");
-  digitalWrite(6, HIGH);
-  for (byte i = 0; i < 3; i++) {
-    digitalWrite(8, HIGH); delay(200);
-    digitalWrite(8, LOW); delay(200);
-  }
-}
-
-void accessDenied() {
-  Serial.println();
-  Serial.println("Message: Access Denied");
-  digitalWrite(7, HIGH);
-  for (byte i = 0; i < 3; i++) {
-    digitalWrite(8, HIGH);
-    delay(1000);
-  }
-}
